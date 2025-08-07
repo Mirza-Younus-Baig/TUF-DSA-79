a423919587c4f0f2eefc1f5d679f4c703cce5aa2
Mirza Younus Baig <mirzayounus40@gmail.com>
2025-08-07 16:11:00 +0530

    count inversion

--- Count inversions.py
+++ Count inversions.py
@@ -1,4 +1,8 @@
 class Solution:
+    # declare as a global variable or a class variable
+    def __init__(self):
+        self.cnt = 0
+
     def mergeSort(self, arr, low, high):
         if low < high:
             mid = (low + high) // 2
@@ -19,7 +23,8 @@ class Solution:
             else:
                 res.append(arr[j])
                 j += 1
-                cnt += 1
+                # add all the no of elements from the left array
+                self.cnt += mid - i + 1
             
         while i <= mid:
             res.append(arr[i])
@@ -31,12 +36,9 @@ class Solution:
         for i in range(low, high + 1):
             arr[i] = res[i - low]
 
-        
-
-
 
 obj = Solution()
 arr = [4,3,2,5,1,8,7,9,6]
 obj.mergeSort(arr, 0, len(arr) - 1)
-print("Sorted array:", arr)
+print("Number of inversion pairs:", obj.cnt)
 
