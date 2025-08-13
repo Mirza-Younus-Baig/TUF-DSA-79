90c5a47e94b2138562d2164fece293eabacc38e3
Mirza Younus Baig <mirzayounus40@gmail.com>
2025-08-13 06:14:50 +0530

    wip

---
+++ Agressive Cows.py
@@ -0,0 +1,11 @@
+class Solution:
+    def aggressiveCows(self, nums, k):
+        
+        
+        return res
+
+
+obj = Solution()
+nums =  [0, 3, 4, 7, 10, 9]
+k = 4
+print(obj.aggressiveCows(nums, k))
